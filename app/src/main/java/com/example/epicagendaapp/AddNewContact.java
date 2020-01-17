b0af3f3575e8f9956e157c98cc3ed380a497010e
Miki <jaworski.miki98@gmail.com>
2020-01-17 16:49:45 +0100

    he fet moltes coses

---
+++ app/src/main/java/com/example/epicagendaapp/AddNewContact.java
@@ -0,0 +1,14 @@
+package com.example.epicagendaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+    }
+}
