6802d257f597d2f07d6cbae2d1c239d75108807d
Calamar2 <saroca8@boscdelacoma.cat>
2020-01-17 16:46:17 +0100

    naps i cols

--- app/src/main/java/com/example/epicagendaapp/MainActivity.java
+++ app/src/main/java/com/example/epicagendaapp/MainActivity.java
@@ -4,25 +4,278 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.epicagendaapp.ui.login.LoginFirebase;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnChangeEmail, btnChangePassword, btnSendResetEmail, btnRemoveUser,
+            changeEmail, changePassword, sendEmail, remove, signOut;
+    private EditText oldEmail, newEmail, password, newPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        // Obtener referencia de Auth Firebase
+        auth = FirebaseAuth.getInstance();
+        // Obtener usuario actual
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // Estado de autenticación del usuario cambiado - el usuario es nulo
+                    // Iniciar actividad de inicio de sesión
+                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        // Obtener referencias de los objetos
+        btnChangeEmail =  findViewById(R.id.change_email_button);
+        btnChangePassword = findViewById(R.id.change_password_button);
+        btnSendResetEmail = findViewById(R.id.sending_pass_reset_button);
+        btnRemoveUser = findViewById(R.id.remove_user_button);
+        changeEmail = findViewById(R.id.changeEmail);
+        changePassword = findViewById(R.id.changePass);
+        sendEmail = findViewById(R.id.send);
+        remove = findViewById(R.id.remove);
+        signOut = findViewById(R.id.sign_out);
+
+        oldEmail = findViewById(R.id.old_email);
+        newEmail = findViewById(R.id.new_email);
+        password = findViewById(R.id.password);
+        newPassword = findViewById(R.id.newPassword);
+
+        oldEmail.setVisibility(View.GONE);
+        newEmail.setVisibility(View.GONE);
+        password.setVisibility(View.GONE);
+        newPassword.setVisibility(View.GONE);
+        changeEmail.setVisibility(View.GONE);
+        changePassword.setVisibility(View.GONE);
+        sendEmail.setVisibility(View.GONE);
+        remove.setVisibility(View.GONE);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+        // Clic para cambiar correo electronico oculta y habilita los controles
+        btnChangeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oldEmail.setVisibility(View.GONE);
+                newEmail.setVisibility(View.VISIBLE);
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.GONE);
+                changeEmail.setVisibility(View.VISIBLE);
+                changePassword.setVisibility(View.GONE);
+                sendEmail.setVisibility(View.GONE);
+                remove.setVisibility(View.GONE);
+            }
+        });
+        // Cambiar el correco electronico
+        changeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar progressbar
+                progressBar.setVisibility(View.VISIBLE);
+                // Validar el que correo no este vacio
+                if (user != null && !newEmail.getText().toString().trim().equals("")) {
+                    user.updateEmail(newEmail.getText().toString().trim())
+                            .addOnCompleteListener(new OnCompleteListener() {
+                                @Override
+                                public void onComplete(@NonNull Task task) {
+                                    // Muestra un mensaje si la actualización fue correcta de lo contrario mostrara que no se pudo actualizar
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "La dirección de correo electrónico está actualizada. Por favor inicie sesión con una nueva identificación de correo electrónico!", Toast.LENGTH_LONG).show();
+                                        signOut();
+                                        progressBar.setVisibility(View.GONE);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "¡No se pudo actualizar el correo electrónico!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                } else if (newEmail.getText().toString().trim().equals("")) {
+                    newEmail.setError("Ingrese correo electronico!");
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        // Clic para cambiar contraseña habilida y oculta controles
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oldEmail.setVisibility(View.GONE);
+                newEmail.setVisibility(View.GONE);
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.VISIBLE);
+                changeEmail.setVisibility(View.GONE);
+                changePassword.setVisibility(View.VISIBLE);
+                sendEmail.setVisibility(View.GONE);
+                remove.setVisibility(View.GONE);
+            }
+        });
+        // Cambiar contraseña
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar progressbar
+                progressBar.setVisibility(View.VISIBLE);
+                // Verificar si la contraseña no esta vacia
+                if (user != null && !newPassword.getText().toString().trim().equals("")) {
+                    // Verificar si la nueva constraseña no esta vacia
+                    if (newPassword.getText().toString().trim().length() < 6) {
+                        newPassword.setError("Contraseña demasiado corta, ingrese un mínimo de 6 caracteres");
+                        progressBar.setVisibility(View.GONE);
+                    } else {
+                        // Actualizar contraseña
+                        user.updatePassword(newPassword.getText().toString().trim())
+                                .addOnCompleteListener(new OnCompleteListener() {
+                                    @Override
+                                    public void onComplete(@NonNull Task task) {
+                                        // Mostrar mensaje si se logro actualizar la contraseña
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(MainActivity.this, "La contraseña se actualiza, inicia sesión con una nueva contraseña.", Toast.LENGTH_SHORT).show();
+                                            signOut();
+                                            progressBar.setVisibility(View.GONE);
+                                        } else {
+                                            Toast.makeText(MainActivity.this, "Error al actualizar la contraseña!", Toast.LENGTH_SHORT).show();
+                                            progressBar.setVisibility(View.GONE);
+                                        }
+                                    }
+                                });
+                    }
+                } else if (newPassword.getText().toString().trim().equals("")) {
+                    newPassword.setError("Ingrese la contraseña");
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        // Clic para resetear contraseña de un email
+        btnSendResetEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oldEmail.setVisibility(View.VISIBLE);
+                newEmail.setVisibility(View.GONE);
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.GONE);
+                changeEmail.setVisibility(View.GONE);
+                changePassword.setVisibility(View.GONE);
+                sendEmail.setVisibility(View.VISIBLE);
+                remove.setVisibility(View.GONE);
+            }
+        });
+        // Clic para resetear la contraseña
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar progressbar
+                progressBar.setVisibility(View.VISIBLE);
+                // Validar que el correo electronico fue ingresado
+                if (!oldEmail.getText().toString().trim().equals("")) {
+                    // Resetear la contraseña del correo
+                    auth.sendPasswordResetEmail(oldEmail.getText().toString().trim())
+                            .addOnCompleteListener(new OnCompleteListener() {
+                                @Override
+                                public void onComplete(@NonNull Task task) {
+                                    // Mostrar un mensaje si se logro resetear la contraseña o si ocurrio un problema
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Restablecer la contraseña de correo electrónico se envía!", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Error al enviar el correo electrónico de reinicio!", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                } else {
+                    oldEmail.setError("Ingresar correo electronico!");
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        // Clic para eliminar correo electronico
+        btnRemoveUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar progressbar
+                progressBar.setVisibility(View.VISIBLE);
+                // Validar que el usuario no este vacio
+                if (user != null) {
+                    user.delete()
+                            .addOnCompleteListener(new OnCompleteListener() {
+                                @Override
+                                public void onComplete(@NonNull Task task) {
+                                    // Mostrar un mensaje si se logro eliminar el correo electronico o si ocurrio un problema
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Tu perfil ha sido eliminado :( ¡Crea una cuenta ahora!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                                        finish();
+                                        progressBar.setVisibility(View.GONE);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "¡No se borró tu cuenta!", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+        // Cerrar sesion del usuario
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
     }
 
-    public void OpenMobileContacts(View view) {
-        Intent intent = new Intent(this, ContactsTelActivity.class);
-        startActivity(intent);
+    // Método de cierre de sesión
+    public void signOut() {
+        auth.signOut();
     }
 
-    public void OpenFirebaseContacts(View view) {
-        Intent intent = new Intent(this, LoginFirebase.class);
-        startActivity(intent);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 }
