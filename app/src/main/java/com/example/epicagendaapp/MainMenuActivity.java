7aec65427f31acddfb71c0810a1892ced5ad23e4
Miki <jaworski.miki98@gmail.com>
2020-01-21 16:02:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/epicagendaapp/MainMenuActivity.java
+++ app/src/main/java/com/example/epicagendaapp/MainMenuActivity.java
@@ -1,17 +1,56 @@
 package com.example.epicagendaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    Button mobileContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        mobileContacts = findViewById(R.id.contacts_mobile);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(this, "Permission granted to read your contacts.", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied to read your contacts.", Toast.LENGTH_SHORT).show();
+                    mobileContacts.setEnabled(false);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     public void OpenMobileContacts(View view) {
